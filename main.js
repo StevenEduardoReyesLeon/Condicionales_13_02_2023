cd8be15a7d2a10892dbbd942bdba3dca9071470f
StevenEduardoReyesLeon <stevenrl325@gmail.com>
2023-02-16 21:18:06 -0500

    Ejemplo Condicional IF ELSE

--- main.js
+++ main.js
@@ -1,35 +1,15 @@
+let hora = prompt("Ingrese la hora por favor");
 
-alert("Abrir por favor la consola o presiona F12")
+if (hora>=6 && hora<=12){
 
-if(true){
-    let num1 = Number(prompt("Ingrese el numero 1"));
-    let num2 = Number(prompt("Ingrese el numero 2"));
-    console.log(num1+num2);
-    console.log("Condicion Correcta");
+    alert("Buenos días!");
+
+}else if(hora>=13 && hora<=18){
+
+    alert("Buenas tardes!");
+
 }else{
-    console.error("Condicion Incorrecta :(");
+
+    alert("Buenas noches!");
 }
 
-// ------------------------------------------------
-
-if(num>=5 && num<=10){
-    alert(`Usuario el numero ${num} ingresado es el correcto`);
-}else{
-    console.error(`Usuario el numero ${num} ingresado es el incorrecto`);
-}
-
-// ------------------------------------------------
-
-let num = number(prompt("Usuario ingrese un numero entres 5 - 10"));
-
-if(num<=5){
-    if(num>=10){
-        console.log(`Usuario el numero ${num} ingresado es el correcto`);
-    }else{
-        console.error(`Usuario el numero ${num} ingresado es el incorrecto`)
-    }
-}else{
-    console.error(`Usuario el numero ${num} ingresado es el incorrecto`)
-}
-
-   
